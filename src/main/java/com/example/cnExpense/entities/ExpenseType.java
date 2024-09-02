83b0330268bcca90175186190da781c4a13a6182
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-02 12:48:48 +0530

    adding field properties for ExpenseType

--- src/main/java/com/example/cnExpense/entities/ExpenseType.java
+++ src/main/java/com/example/cnExpense/entities/ExpenseType.java
@@ -4,5 +4,38 @@ import javax.persistence.Entity;
 
 @Entity
 public class ExpenseType {
+    private int id;
+
+    private String name;
+
+    private Expense expense;
+
+    public ExpenseType() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Expense getExpense() {
+        return expense;
+    }
+
+    public void setExpense(Expense expense) {
+        this.expense = expense;
+    }
+
     
 }
