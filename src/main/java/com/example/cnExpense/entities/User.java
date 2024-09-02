d2a509b008c8458230e20699b0c94aac1603c1ff
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-02 12:26:59 +0530

    adding field properties for User

--- src/main/java/com/example/cnExpense/entities/User.java
+++ src/main/java/com/example/cnExpense/entities/User.java
@@ -1,8 +1,118 @@
 package com.example.cnExpense.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 @Entity
 public class User {
+    
+    private  Integer id;
+
+    
+    private  String username;
+
+    
+    private  String nickname;
+
+    
+    private  String email;
+
+    
+    private  String address;
+
+    
+
+    private  List<Expense> expenses;
+
+      
+
+    private  List<Income> incomes;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+    public String getNickname() {
+        return nickname;
+    }
+
+
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public String getAddress() {
+        return address;
+    }
+
+
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+
+
+    public List<Income> getIncomes() {
+        return incomes;
+    }
+
+
+
+    public void setIncomes(List<Income> incomes) {
+        this.incomes = incomes;
+    }
+
     
 }
