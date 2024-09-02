61cab0d225263916d6f2d0c09c48b482364cfcd4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-02 12:34:00 +0530

    adding field properties for Expense

--- src/main/java/com/example/cnExpense/entities/Expense.java
+++ src/main/java/com/example/cnExpense/entities/Expense.java
@@ -1,8 +1,85 @@
 package com.example.cnExpense.entities;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.persistence.Entity;
 
 @Entity
 public class Expense {
+    private int id;
+
+    private double amount;
+
+    private LocalDate date;
+
+    private String description;
+
+    private User user;
+
+    private Income income;
+
+    private List<ExpenseType> expenseTypes;
+
+    public Expense() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Income getIncome() {
+        return income;
+    }
+
+    public void setIncome(Income income) {
+        this.income = income;
+    }
+
+    public List<ExpenseType> getExpenseTypes() {
+        return expenseTypes;
+    }
+
+    public void setExpenseTypes(List<ExpenseType> expenseTypes) {
+        this.expenseTypes = expenseTypes;
+    }
+
     
+
 }
