4c78d39b4889d6d95262984b218ca210bc6cff2e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-03 22:19:47 +0530

    changing user service method impl

--- src/main/java/com/example/cnExpense/service/UserService.java
+++ src/main/java/com/example/cnExpense/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.cnExpense.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.cnExpense.DAL.UserDAL;
+import com.example.cnExpense.entities.ExpenseType;
+import com.example.cnExpense.entities.Income;
+import com.example.cnExpense.entities.IncomeType;
 import com.example.cnExpense.entities.User;
 import com.example.cnExpense.exception.ElementAlreadyExistException;
 import com.example.cnExpense.exception.NotFoundException;
@@ -38,6 +42,11 @@ public class UserService {
     }
 
     @Transactional
+    public boolean checkUserExists(User user) {
+        return userDAL.checkUserExist(user);
+    }
+
+    @Transactional
     public void saveUser(User user){
         User u = userDAL.getUserById(user.getId());
         if (u != null) {
@@ -47,36 +56,73 @@ public class UserService {
         userDAL.saveUser(user);
     }
 
-    @Transactional
-    public boolean checkUserExists(User user){
-        return userDAL.checkUserExists(user);
-    }
+    
 
     @Transactional
     public User findUser(User user){
-        User u = userDAL.findUser(user);
-        if (u == null) {
-            return null;
-        }
-
-        return user;
+        return userDAL.findUser(user);
     }
 
     @Transactional
     public List<User> UserListByCalendar(String day, String month, String year){
-        List<User> list = userDAL.UserListByCalendar(day,month,year);
-        if (list.isEmpty()) {
-            throw new NotFoundException("no user list find by Calender");
+        List<User> filteredList = new ArrayList<>();
+        List<User> userList= getAllUsers();
+        for (User user : userList) {
+            List<Income> filteredIncomes = new ArrayList<>();
+            for (Income income : user.getIncomes()) {
+                if ((income.getDate()!=null) && (day != null && !day.isEmpty() && !(Integer.parseInt(day)==income.getDate().getDayOfMonth()))) {
+                    continue;
+                }
+                else if ((income.getDate()!=null) && (month != null && !month.isEmpty() && !(Integer.parseInt(month)==income.getDate().getMonthValue()))) {
+                    continue;
+                }
+                else if ((income.getDate()!=null) && (year != null && !year.isEmpty() && !(Integer.parseInt(year)==income.getDate().getYear()))) {
+                    continue;
+                }
+                filteredIncomes.add(income);
+            }
+            user.setIncomes(filteredIncomes);
+            filteredList.add(user);
         }
-        return list;
+        return filteredList;
     }
 
     @Transactional
     public List<User> UserListByType(String incomeType, String expenseType){
-        List<User> list = userDAL.UserListByType(incomeType,expenseType);
-        if (list.isEmpty()) {
-            throw new NotFoundException("no user list find by income and expense type");
+        List<User> userList = userDAL.getAllUsers();
+        List<User> filteredList = new ArrayList<>();
+        for (User user : userList) {
+            List<Income> filteredIncomes = new ArrayList<>();
+            for (Income income : user.getIncomes()) {
+                if (incomeType != null && !incomeType.isEmpty()) {
+                    boolean foundIncomeType = false;
+                    for (IncomeType type : income.getIncomeTypes()) {
+                        if (type.getName().equalsIgnoreCase(incomeType)) {
+                            foundIncomeType = true;
+                            break;
+                        }
+                    }
+                    if (!foundIncomeType) {
+                        continue;
+                    }
+                }
+                else if (expenseType != null && !expenseType.isEmpty()) {
+                    boolean foundExpenseType = false;
+                    for (ExpenseType type : income.getExpense().getExpenseTypes()) {
+                        if (type.getName().equalsIgnoreCase(expenseType)) {
+                            foundExpenseType = true;
+                            break;
+                        }
+                    }
+                    if (!foundExpenseType) {
+                        continue;
+                    }
+                }
+                filteredIncomes.add(income);
+            }
+            user.setIncomes(filteredIncomes);
+            filteredList.add(user);
         }
-        return list;
+        return filteredList;
     }
 }
