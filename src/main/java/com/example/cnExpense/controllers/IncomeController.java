551101eb9cd9821530b5f1da0ad2469520c1d122
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-03 15:09:09 +0530

    adding Income controller

---
+++ src/main/java/com/example/cnExpense/controllers/IncomeController.java
@@ -0,0 +1,30 @@
+package com.example.cnExpense.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cnExpense.entities.Income;
+import com.example.cnExpense.service.IncomeService;
+
+@RestController
+@RequestMapping("/incomes")
+public class IncomeController {
+    
+    @Autowired
+    IncomeService incomeService;
+
+    @GetMapping("/{incomeid}")
+    public Income getIncomeById(@PathVariable Integer incomeid){
+        return incomeService.getIncomeById(incomeid);
+    }
+
+    @PostMapping("/save/{userId}")
+    public void saveIncomeForUser(@PathVariable Integer userId, @RequestBody Income income){
+        incomeService.saveIncomeForUser(userId,income);
+    }
+}
